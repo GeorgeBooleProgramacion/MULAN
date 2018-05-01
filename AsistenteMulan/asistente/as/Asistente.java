5b023747e09e2180de73ec4163608db5d98ac047
Fiordalisi <te.fiordalisi@gmail.com>
2018-05-01 01:18:03 -0300

    Wip4

--- AsistenteMulan/asistente/as/Asistente.java
+++ AsistenteMulan/asistente/as/Asistente.java
@@ -1,5 +1,36 @@
 package as;
 
 public class Asistente {
-
+	
+	private static final String[] REG_SALUDO = {"¡Hola, ", "Buen dia", "Buenas Tardes", "Hola", 
+												"hey ", "buen día ", " hola", ", buenas tardes"};
+	private static final String[] REG_SALU2 = {"¡Hola, ", "buen día ", "hey "};
+	private String nombre;
+	
+	public Asistente(String name) {
+		this.nombre = name;
+	}
+	
+	public String getNombre() {
+		return this.nombre;
+	}
+	
+	public String escuchar(String saludo) {
+		for(int i=0; i < REG_SALU2.length; i++) {
+			if((REG_SALU2[i]+"@"+this.nombre).equals(saludo) )
+				return "¡Hola, USUARIO"; 
+		}
+//		for(int i=0; i < REG_SALUDO.length; i++) {
+//			if((REG_SALUDO[i].toUpperCase()+"@"+this.nombre).equals(saludo.toUpperCase()) )
+//				return "Hola, "+"USUARIO"; 
+//		}
+//		for(int i=0; i < REG_SALUDO.length; i++) {
+//			if(("@"+this.nombre+REG_SALUDO[i].toUpperCase()).equals(saludo.toUpperCase()) )
+//				return "Hola, "+"USUARIO"; 
+//		}
+		return "No entiendo";
+	}
+	
+	
+	
 }
--- AsistenteMulan/test/t/PruebaMulan.java
+++ AsistenteMulan/test/t/PruebaMulan.java
@@ -6,17 +6,46 @@ import org.junit.Test;
 
 import en.BufferEntrada;
 import as.Mulan;
+import as.Asistente;
 
 public class PruebaMulan {
+	public final static String USUARIO = "delucas"; 
 	
+//	@Test
+//	public void testSaludo() {
+//		BufferEntrada b;
+//		do{
+//			b = new BufferEntrada();
+//		}while (Mulan.charlar(b.getEntrada()));
+//	}
+
+	Asistente jenkins;
+	
+	@Before
+	public void setup() {
+		jenkins = new Asistente("jenkins");
+	}
 	@Test
-	public void testSaludo() {
-		BufferEntrada b;
-		do{
-			b = new BufferEntrada();
-		}while (Mulan.charlar(b.getEntrada()));
+	public void testNombre() {
+		Assert.assertEquals("jenkins",jenkins.getNombre());
 	}
 	
-	//Esto deberia funcionar???
-
+	@Test
+	public void testSaludo2() {
+		String[] mensajes = {
+				"¡Hola, @jenkins",
+				//"@jenkins hola!",
+				"buen día @jenkins",
+//				"@jenkins, buenas tardes",
+				"hey @jenkins"
+		};
+		
+		//String msj = "¡Hola, @jenkins";
+		
+		//Assert.assertEquals("¡Hola, USUARIO",jenkins.escuchar(msj));
+		for (String mensaje : mensajes) {
+			Assert.assertEquals("¡Hola, USUARIO",jenkins.escuchar(mensaje));
+	}
+	}
 }
+
